2fc0632f3c2d380b9787b3053b8181fa2e77dcfb
Anton McConville <anton@hickory.ca>
2020-02-14 16:10:04 -0500

    web templates

---
+++ public/javascript/welcome.js
@@ -0,0 +1,39 @@
+class Welcome extends HTMLElement {
+
+    constructor() {
+        // Always call super first in constructor
+        super();
+
+        // write element functionality in here
+
+        // var shadow = this.attachShadow({
+        //     mode: 'open'
+        // });
+
+        // Create spans
+        // var wrapper = document.createElement('span');
+        // wrapper.setAttribute('class', 'wrapper');
+        
+        // wrapper.innerHTML = view;
+        // Take attribute content and put it inside the info span
+        // var text = this.getAttribute('text');
+        // info.textContent = text;
+
+        let template = document.getElementById('welcomeview');
+        let templateContent = template.content;
+  
+        const shadow = this.attachShadow({mode: 'open'})
+          .appendChild(templateContent.cloneNode(true));
+
+        // shadow.appendChild(wrapper);
+        // wrapper.appendChild(info);
+    }
+}
+
+try {
+    customElements.define('welcome-element', Welcome);
+} catch (err) {
+    const h3 = document.createElement('h3')
+    h3.innerHTML = err
+    document.body.appendChild(h3)
+}
