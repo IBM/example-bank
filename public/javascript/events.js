ccf4b4cac66b70289a01e42ceaf223970e7231c8
Anton McConville <anton@hickory.ca>
2020-03-02 16:10:21 -0500

    data fed version of events

--- public/javascript/events.js
+++ public/javascript/events.js
@@ -1,8 +1,30 @@
 class Events extends HTMLElement {
 
+    createLabel(classname, content) {
+        var label = document.createElement('label');
+        label.className = classname;
+        label.innerHTML = content;
+        return label;
+    }
+
+    showEvents(eventdata) {
+
+        var sr = this.shadowRoot;
+        var anchor = sr.getElementById('eventanchor');
+        var component = this;
+
+        eventdata.forEach(function (event) {
+            var evt = document.createElement('div');
+            evt.className = 'eventblock';
+            evt.appendChild(component.createLabel('eventname', event.title));
+            evt.appendChild(component.createLabel('eventdate', event.date));
+            evt.appendChild(component.createLabel('eventpoints', event.points + ' EVENT POINTS'));
+            evt.appendChild(component.createLabel('eventaddress', event.address));
+            anchor.appendChild(evt);
+        })
+    }
 
     constructor() {
-        // Always call super first in constructor
         super();
 
         let template = document.getElementById('eventview');
@@ -14,6 +36,35 @@ class Events extends HTMLElement {
                 mode: 'open'
             })
             .appendChild(templateContent.cloneNode(true));
+    }
+
+    connectedCallback() {
+
+        /* first retrieve the event data here*/
+
+        var sampledata = [{
+            "title": "CITYJSCONF",
+            "date": "MARCH 27, 2020",
+            "points": 15,
+            "address": "REGENT STREET THEATRE, LONDON"
+        }, {
+            "title": "BINARY DISTRICT ZERO PROOFS WORKSHOP",
+            "date": "JUNE 12, 2020",
+            "points": 15,
+            "address": "TRUMAN BREWERY, BRICK LANE, E1"
+        }, {
+            "title": "CITYJSCONF",
+            "date": "MARCH 27, 2020",
+            "points": 15,
+            "address": "REGENT STREET THEATRE, LONDON"
+        }, {
+            "title": "BINARY DISTRICT ZERO PROOFS WORKSHOP",
+            "date": "JUNE 12, 2020",
+            "points": 15,
+            "address": "TRUMAN BREWERY, BRICK LANE, E1"
+        }]
+
+        this.showEvents(sampledata);
     }
 }
 
