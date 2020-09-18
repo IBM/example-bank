34baff62b3f6c549c94df630c64dc59734c76130
koyfman <koyfman@us.ibm.com>
2020-09-18 17:09:09 -0400

    Cleanup script update

--- scripts/deleteresources.sh
+++ scripts/deleteresources.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
+
+
+
+# Delete resources before removing namespace
+oc delete all --all -n example-bank
+sleep 2
 
 ## begin delete openshift serverless
-
 # delete knative-serving component
 oc delete knativeservings.operator.knative.dev knative-serving -n knative-serving
 oc delete namespace knative-serving
@@ -15,7 +20,25 @@ oc delete csv $(oc get csv -n openshift-operators -o=jsonpath='{.items[*].metada
 ## end of delete openshift serverless
 
 
+# Service mesh cleanup
+
+oc delete smcp --all -n istio-system
+oc delete smmr --all -n istio-system
 
 
-## this should delete example bank resources
+oc delete validatingwebhookconfiguration/openshift-operators.servicemesh-resources.maistra.io
+oc delete mutatingwebhookconfigurations/openshift-operators.servicemesh-resources.maistra.io
+oc delete -n openshift-operators daemonset/istio-node
+oc delete clusterrole/istio-admin clusterrole/istio-cni clusterrolebinding/istio-cni
+oc delete subs --all --all-namespaces
+oc get crds -o name | grep '.*\.istio\.io' | xargs -r -n 1 oc delete
+oc get crds -o name | grep '.*\.maistra\.io' | xargs -r -n 1 oc delete
+oc get crds -o name | grep '.*\.kiali\.io' | xargs -r -n 1 oc delete
+
+oc delete all --all -n istio-system
+sleep 2
+
+## Delete projects
+
 oc delete project example-bank
+oc delete project istio-system
